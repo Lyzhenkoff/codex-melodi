fa84bb19f6b3a59ee1ef260a5496a10927010dc4
Kirilllert <46005727+Kirilllert@users.noreply.github.com>
2019-10-07 00:17:57 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -90,30 +90,24 @@ var playNote = function (frequency, startTime, duration) {
         osc2 = context.createOscillator(),
         volume = context.createGain();
  
-    // Set oscillator wave type
     osc1.type = 'triangle';
     osc2.type = 'triangle';
  
     volume.gain.value = 0.1;    
- 
-    // Set up node routing
+
     osc1.connect(volume);
     osc2.connect(volume);
     volume.connect(context.destination);
  
-    // Detune oscillators for chorus effect
     osc1.frequency.value = frequency + 1;
     osc2.frequency.value = frequency - 2;
  
-    // Fade out
     volume.gain.setValueAtTime(0.1, startTime + duration - 0.05);
     volume.gain.linearRampToValueAtTime(0, startTime + duration);
  
-    // Start oscillators
     osc1.start(startTime);
     osc2.start(startTime);
  
-    // Stop oscillators
     osc1.stop(startTime + duration);
     osc2.stop(startTime + duration);
 };
