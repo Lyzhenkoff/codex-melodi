5bfc9893c5a78edaeebcf0e36fcd950b980e5778
Kirilllert <46005727+Kirilllert@users.noreply.github.com>
2019-10-09 23:59:23 +0300

    summary

--- webpack-config.js
+++ webpack-config.js
@@ -1,27 +1,53 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const conf {
-	entry: 'src/script.js',
-	output: {
-		path: path.resolve(__dirname,'dist'),
-		filename: 'main.js',
-		publicPath: 'dist'
-	},
-	devServer:{
-		overlay: true
-	},
-	module: {
-		rules: [
-			{
-				test:/\.css$/,
-				use:[
-				'style-loader',
-				'css-loader'
-				]
+const conf;
+{
+  entry: 'src/script.js',
+    output;
+:
+  {
+    path: path.resolve(__dirname, 'dist'),
+      filename;
+  :
+    'main.js',
+      publicPath;
+  :
+    'dist';
+  }
+,
+  devServer:{
+    overlay: true;
+  }
+,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../'
+            }
+          },
+          'css-loader',
+          'postcss-loader'
+        ]
+      }
+    ];
+  }
+,
 
-			}
-		]
-	}
-};
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      filename: 'dist/bundle.css'
+    })
+  ],
+}
+;
 
 module.exports = conf;
