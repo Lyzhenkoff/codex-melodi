930e8c526df6c54dc914ab93ccab8cd3effe8e9c
Kirilllert <46005727+Kirilllert@users.noreply.github.com>
2019-10-13 23:28:12 +0300

    change webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,56 +2,58 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const conf = {
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'main.js',
-            publicPath: 'dist'
-        },
-        devServer: {
-            overlay: true
-        },
-        module: {
-            rules: [
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+      publicPath: 'dist'
+    },
+    devServer: {
+      overlay: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },  {
-                    test: /\.css$/,
-                    use: [
-                        {
-                            loader: MiniCssExtractPlugin.loader,
-                            options: {
-                                // you can specify a publicPath here
-                                // by default it use publicPath in webpackOptions.output
-                                publicPath: '../'
-                            }
-                        }
-                        'css-loader'
-                    ]
-                }
-            ]
-        },
+              loader: 'babel-loader',
+              query: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+            {
+              loader: 'eslint-loader'
+              options: {
+                fix: true
+              }
+            }
+          ]
+        }, {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // you can specify a publicPath here
+                // by default it use publicPath in webpackOptions.output
+                publicPath: '../'
+              }
+            },
+            'css-loader'
+          ]
+        }]
+    },
 
 
-        plugins: [
-            new MiniCssExtractPlugin({
-                // Options similar to the same options in webpackOptions.output
-                filename: 'bundle.css'
-            })
-        ],
-    }
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        filename: 'bundle.css'
+      })
+    ],
+  }
 ;
 
 module.exports = conf;
